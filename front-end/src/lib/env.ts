86e21bc694a233fd8cb76bf5744956cce1216492
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-15 18:17:48 -0300

    gkissel : feat add ky
    http client

--- front-end/src/lib/env.ts
+++ front-end/src/lib/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  BACKEND_URL: z.string().url(),
+  BACKEND_API_URL: z.string().url().optional().default('http://localhost:3333'),
 })
 
 export const env = envSchema.parse(process.env)
