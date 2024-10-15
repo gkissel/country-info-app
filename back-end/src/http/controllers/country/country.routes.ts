f0c563d6092a9e6c522b258aa1d7ce5e9a2e97ab
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-15 16:47:18 -0300

    gkissel : feat add get country info route

--- back-end/src/http/controllers/country/country.routes.ts
+++ back-end/src/http/controllers/country/country.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { GetAllAvailableCountriesController } from './get-available-countries.controller'
+import { GetCountryInfoController } from './get-county-info.controller'
 
 const CountryRouter = Router({
   mergeParams: true,
@@ -8,8 +9,14 @@ const CountryRouter = Router({
 const getAllAvailableCountriesController =
   new GetAllAvailableCountriesController()
 
+const getCountryInfoController = new GetCountryInfoController()
+
 CountryRouter.get('/', (req, res) => {
   getAllAvailableCountriesController.execute(req, res)
+})
+
+CountryRouter.get('/:countryCode', (req, res) => {
+  getCountryInfoController.execute(req, res)
 })
 
 export default CountryRouter
