4715edf8f230dc0e37e465a7e4080c403cf53e18
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-15 15:50:05 -0300

    gkissel : feat add initial structure

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import * as bodyParser from 'body-parser'
+import express from 'express'
+
+import { env } from './app/env'
+export const app = express()
+
+app.use(bodyParser.json())
+
+app.listen(env.PORT)
+
+// app.use('/api/v1/', routes)
+
+// app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+console.log(`Express server started on port ${env.PORT}`)
