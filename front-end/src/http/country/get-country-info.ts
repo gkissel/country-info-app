4be21a890ac656c2ed50fa8e5161e5bfc172289c
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-15 18:25:20 -0300

    gkissel : feat add get country info

---
+++ front-end/src/http/country/get-country-info.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+import { api } from '@/lib/api-client'
+
+const getCountryInfoSchema = z.object({
+  borders: z.array(
+    z.object({
+      commonName: z.string(),
+      officialName: z.string(),
+      countryCode: z.string(),
+      region: z.string(),
+      borders: z.null(),
+    }),
+  ),
+  populationCounts: z.array(z.object({ year: z.number(), value: z.number() })),
+  flag: z.string(),
+})
+
+type GetCountryInfoResponse = z.infer<typeof getCountryInfoSchema>
+
+export async function getCountryInfo({ countryCode }: { countryCode: string }) {
+  const result = await api
+    .get<GetCountryInfoResponse>(`countries/${countryCode}`)
+    .json<GetCountryInfoResponse>()
+
+  return result
+}
