b5840e4e3c9bb7576f834717688a1149098f5fd3
Gustavo Kissel <kisselgustavo@gmail.com>
2024-10-15 15:49:41 -0300

    gkissel : feat update eslint config

---
+++ back-end/.eslintrc.js
@@ -0,0 +1,25 @@
+/** @type { import('eslint').Linter.Config } */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
+        endOfLine: 'auto',
+        insertPragma: false,
+      },
+    ],
+    semi: ['error', 'never'],
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+  ignorePatterns: ['package.json'],
+}
